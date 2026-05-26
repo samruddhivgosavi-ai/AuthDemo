2cd1c176d461f7a7537dba76adadb55b3e2f98d1
samruddhivgosavi-ai <samruddhi.vgosavi@gmail.com>
2026-05-26 12:19:41 +0530

    auth

--- auth/myapp/admin.py
+++ auth/myapp/admin.py
@@ -1,3 +1,6 @@
 from django.contrib import admin
-
+from.models import student,address,Skills,candidate,parent,child1,child2
 # Register your models here.
+admin.site.register([student,address])
+admin.site.register([Skills,candidate])
+admin.site.register([child1,child2])
--- auth/myapp/models.py
+++ auth/myapp/models.py
@@ -1,3 +1,43 @@
 from django.db import models
 
 # Create your models here.
+#one to one  ......1 stu=lads
+class student(models.Model):
+    name=models.CharField(max_length=100)
+    contact=models.CharField(max_length=100)
+
+def __str__(self):
+    return self.name
+
+
+class address(models.Model):
+    student=models.OneToOneField(student,on_delete=models.CASCADE) #inherit
+    city=models.CharField(max_length=100)
+    state=models.CharField(max_length=100)
+
+#many to many  ......many stu=many skills
+
+class Skills (models.Model):
+    profile=models.CharField(max_length=100)
+    # contact=models.CharField(max_length=100)
+     
+    def __str__(self):
+        return self.profile
+    
+    class candidate(models.Model):
+        name=models.CharField(max_length=100)
+        contact=models.CharField(max_length=100)
+        Skills=models.ManyToManyField(Skills)
+    
+    class parent(models.Model):
+        name=models.CharField(max_length=100)
+        contact=models.CharField(max_length=100)
+        Skills=models.ManyToManyField(Skills)
+
+        class meta:
+            abstract=True
+    class child1(parent):
+        c1_name=models.CharField(max_length=100)
+
+    class child2(parent):
+        c2_name=models.CharField(max_length=100)
